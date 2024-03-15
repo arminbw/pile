438c472d80c90f067a6120abe85be97cb9c1916c
Armin Wagner <arminbw@users.noreply.github.com>
2024-03-15 23:35:25 +0100

    fixed gulpfile.js (ES6 now)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 import gulp from "gulp";
-import {deleteAsync} from "del";
+import { deleteAsync } from "del";
 import stripComments from "gulp-strip-comments";
 import stripDebug from "gulp-strip-debug";
 
